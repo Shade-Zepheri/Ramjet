6d8f0c43ea02ae884f314f0f3231e384ad1a5c9a
Shade-Zepheri <ziroalpha@gmail.com>
2017-07-06 13:22:30 -0500

    [Main] Fixed compile errors

--- Ramjet.m
+++ Ramjet.m
@@ -8,13 +8,12 @@
 #import "Ramjet-Private.h"
 #import "Logging.h"
 
+#define MEMORYSTATUS_CMD_SET_JETSAM_TASK_LIMIT 6
+
 static int maxRequestedTaskLimit;
 static char maxRequestor[MAX_REQUEST_NAME];
 
-#define MEMORYSTATUS_CMD_SET_JETSAM_TASK_LIMIT 6
-
 typedef int (*dlysm_memoryStatus)(uint32_t command, pid_t pid, uint32_t flags, void *buffer, size_t buffersize);
-
 
 extern int ramjet_updateTaskLimit(int taskLimitMB, char* requester) {
 		return ramjet_updateTaskLimitForPID(taskLimitMB, requester, getpid());
@@ -24,14 +23,13 @@ int updateTaskLimit(int taskLimitMB, char* requester, int pid) {
 	int response = -1;
 
 	dlysm_memoryStatus memoryStatus;
-	void *handle = dlopen(NULL, 0);
-	memoryStatus = *(int*)MSFindSymbol(NULL, "memorystatus_control");
+	memoryStatus = (dlysm_memoryStatus)MSFindSymbol(NULL, "memorystatus_control");
 	if (memoryStatus) {
 			response = memoryStatus(MEMORYSTATUS_CMD_SET_JETSAM_TASK_LIMIT, pid, taskLimitMB, NULL, 0);
 			if (response != 0) {
 					RTLogError(@"Error in setting taskLimit to %d by \"%s\"", taskLimitMB, requester);
 			} else {
-					HBLogInfo(@"Sucessfully set taskLimit to %d b \"%s\"", taskLimitMB, requester);
+					RTLogInfo(@"Sucessfully set taskLimit to %d b \"%s\"", taskLimitMB, requester);
 			}
 	} else {
 			RTLogError(@"Error in creating dlysm_memoryStatus");
@@ -47,13 +45,12 @@ extern int ramjet_updateTaskLimitForPID(int taskLimitMB, char* requester, int pi
 		} else if (taskLimitMB > 1024) {
 				RTLogError(@"Tasklimit is in MB. %d too high", taskLimitMB);
 				return -2;
-		} else if (taskLimitMB < maxRequestedWatermark) {
-				HBlogWarn(@"jetslammed: not updating high watermark to %d, previous requestor %s already updated to %d mb", taskLimitMB, maxRequestor, maxRequestedWatermark);
+		} else if (taskLimitMB < maxRequestedTaskLimit) {
 				RTLogWarn(@"Not updating Tasklimit to %d, previous requestor %s already updated to %d mb", taskLimitMB, maxRequestor, maxRequestedTaskLimit);
 		} else {
 				if (updateTaskLimit(taskLimitMB, requester, pid) == 0) {
 						strncpy(maxRequestor, requester, MAX_REQUEST_NAME-1);
-						maxRequestedWatermark = taskLimitMB;
+						maxRequestedTaskLimit = taskLimitMB;
 				}
 		}
 
