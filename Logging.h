62baf09ac058cdd23f8e58e21db8f92285bb159f
Shade Zepheri <shade@Shades-Mac.local>
2018-12-31 15:17:39 -0600

    [Logging] Update logging

--- Logging.h
+++ Logging.h
@@ -1,27 +1,8 @@
-#include <CoreFoundation/CFLogUtilities.h>
+#import <os/log.h>
 
-/* Log Levels
-enum _CFLogLevel {
-  kCFLogLevelEmergency = 0,
-  kCFLogLevelAlert     = 1,
-  kCFLogLevelCritical  = 2,
-  kCFLogLevelError     = 3,
-  kCFLogLevelWarning   = 4,
-  kCFLogLevelNotice    = 5,
-  kCFLogLevelInfo      = 6,
-  kCFLogLevelDebug     = 7
-};
-*/
+#define RT_LOG_INTERNAL(level, type, ...) os_log_with_type(OS_LOG_DEFAULT, level, "[%{public}s:%{public}d] %{public}@", __BASE_FILE__, __LINE__, [NSString stringWithFormat:__VA_ARGS__])
 
-#define RT_LOG_FORMAT(color) CFSTR("\e[1;3" #color "m[%s] \e[m\e[0;3" #color "m%s:%d\e[m \e[0;30;4" #color "m%s:\e[m %@")
-
-#ifdef __DEBUG__
-    #define RT_LOG_INTERNAL(color, level, type, ...) CFLog(level, RT_LOG_FORMAT(color), "Ramjet", __BASE_FILE__, __LINE__, type, (__bridge CFStringRef)[NSString stringWithFormat:__VA_ARGS__]);
-#else
-    #define RT_LOG_INTERNAL(color, level, type, ...)
-#endif
-
-#define RTLogError(...) RT_LOG_INTERNAL(1, kCFLogLevelError, "ERROR", __VA_ARGS__)
-#define RTLogWarn(...) RT_LOG_INTERNAL(3, kCFLogLevelWarning, "WARN", __VA_ARGS__)
-#define RTLogInfo(...) RT_LOG_INTERNAL(2, kCFLogLevelInfo, "INFO", __VA_ARGS__)
-#define RTLogDebug(...) RT_LOG_INTERNAL(6, kCFLogLevelDebug, "DEBUG", __VA_ARGS__)
+#define RTLogDebug(...) RT_LOG_INTERNAL(OS_LOG_TYPE_DEBUG, "DEBUG", __VA_ARGS__)
+#define RTLogInfo(...) RT_LOG_INTERNAL(OS_LOG_TYPE_INFO, "INFO", __VA_ARGS__)
+#define RTLogWarn(...) RT_LOG_INTERNAL(OS_LOG_TYPE_DEFAULT, "WARN", __VA_ARGS__)
+#define RTLogError(...) RT_LOG_INTERNAL(OS_LOG_TYPE_ERROR, "ERROR", __VA_ARGS__)
