fff8250b51736e3772cdfb7a4abf0499b48b88a7
Shade Zepheri <shade@Shades-Mac.local>
2019-01-02 13:01:17 -0600

    [Logging] Make easier to find

--- Logging.h
+++ Logging.h
@@ -1,6 +1,6 @@
 #import <os/log.h>
 
-#define RT_LOG_INTERNAL(level, type, ...) os_log_with_type(OS_LOG_DEFAULT, level, "[%{public}s:%{public}d] %{public}@", __BASE_FILE__, __LINE__, [NSString stringWithFormat:__VA_ARGS__])
+#define RT_LOG_INTERNAL(level, type, ...) os_log_with_type(OS_LOG_DEFAULT, level, "[RAMJET %{public}s:%{public}d] %{public}@", __BASE_FILE__, __LINE__, [NSString stringWithFormat:__VA_ARGS__])
 
 #define RTLogDebug(...) RT_LOG_INTERNAL(OS_LOG_TYPE_DEBUG, "DEBUG", __VA_ARGS__)
 #define RTLogInfo(...) RT_LOG_INTERNAL(OS_LOG_TYPE_INFO, "INFO", __VA_ARGS__)
