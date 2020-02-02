272fedd6a05a73cbef520f76c38101994d086446
Shade-Zepheri <ziroalpha@gmail.com>
2020-02-02 16:30:40 -0600

    [Global] Better error handling Have xpc pass down any error in task limit setting

--- Ramjet.c
+++ Ramjet.c
@@ -10,13 +10,16 @@ extern kern_return_t ramjet_updateTaskLimit(uint32_t taskLimitMB, char *requeste
 	return ramjet_updateTaskLimitForPID(taskLimitMB, requester, getpid());
 }
 
-static bool sendInfo(uint32_t taskLimitMB, char *requester, pid_t pid) {
+static kern_return_t sendInfo(uint32_t taskLimitMB, char *requester, pid_t pid) {
+	// Set default error
+	__block kern_return_t error = KERN_SUCCESS;
+
 	// Create connection
 	xpc_connection_t connection =  xpc_connection_create_mach_service("com.shade.ramjetd", NULL, XPC_CONNECTION_MACH_SERVICE_PRIVILEGED);
 	if (connection == NULL) {
 		// Couldnt create connection
 		os_log_error(OS_LOG_DEFAULT, "Failed to create xpc connection");
-		return false;
+		return KERN_FAILURE;
 	}
 
 	xpc_connection_resume(connection);
@@ -29,9 +32,17 @@ static bool sendInfo(uint32_t taskLimitMB, char *requester, pid_t pid) {
 
 	// Send over data
 	xpc_object_t data = xpc_data_create(&info, sizeof(info));
-	xpc_connection_send_message(connection, data);
+	xpc_connection_send_message_with_reply(connection, data, NULL, ^(xpc_object_t response) {
+		if (xpc_get_type(response) != XPC_TYPE_INT64) {
+			// Cant parse response
+			error = KERN_INVALID_OBJECT;
+		} else {
+			// Get reply of failure status
+			error = xpc_int64_get_value(response);
+		}
+	});
 
-	return true;
+	return error;
 }
 
 extern kern_return_t ramjet_updateTaskLimitForPID(uint32_t taskLimitMB, char *requester, pid_t pid) {
@@ -44,9 +55,9 @@ extern kern_return_t ramjet_updateTaskLimitForPID(uint32_t taskLimitMB, char *re
 	} else if (taskLimitMB < maxRequestedTaskLimit) {
 		os_log_info(OS_LOG_DEFAULT, "Not updating Tasklimit to %u, previous requester %s already set it to %u mb", taskLimitMB, maxRequester, maxRequestedTaskLimit);
 	} else {
-		if (!sendInfo(taskLimitMB, requester, pid)) {
+		if (sendInfo(taskLimitMB, requester, pid) != KERN_SUCCESS) {
 			// Couldnt send info
-			os_log_error(OS_LOG_DEFAULT, "Couldn't communicate with daemon");
+			os_log_error(OS_LOG_DEFAULT, "Error in setting task limit");
 			return KERN_FAILURE;
 		}
 
--- daemon/main.c
+++ daemon/main.c
@@ -7,11 +7,11 @@
 static int updateTaskLimit(uint32_t taskLimitMB, char *requester, pid_t pid) {
 	// Set high water mark
 	int response = memorystatus_control(MEMORYSTATUS_CMD_SET_JETSAM_HIGH_WATER_MARK, pid, taskLimitMB, NULL, 0);
-		if (response != 0) {
-			os_log_error(OS_LOG_DEFAULT, "Error in setting taskLimit to %u by \"%s\" error: %s", taskLimitMB, requester, strerror(errno));
-		} else {
-			os_log_info(OS_LOG_DEFAULT, "Successfully set taskLimit to %u by \"%s\"", taskLimitMB, requester);
-		}
+	if (response != 0) {
+		os_log_error(OS_LOG_DEFAULT, "Error in setting taskLimit to %u by \"%s\" error: %s", taskLimitMB, requester, strerror(errno));
+	} else {
+		os_log_info(OS_LOG_DEFAULT, "Successfully set taskLimit to %u by \"%s\"", taskLimitMB, requester);
+	}
 
 	return response;
 }
@@ -20,26 +20,47 @@ int main(int argc, const char *argv[]) {
 	// Create connection
 	xpc_connection_t connection = xpc_connection_create_mach_service("com.shade.ramjetd", NULL, XPC_CONNECTION_MACH_SERVICE_LISTENER);
 
-	// Set handler
-	xpc_connection_set_event_handler(connection, ^(xpc_object_t object) {
-		if (xpc_get_type(object) != XPC_TYPE_DATA) {
-			// Not data, cant work
-			os_log_error(OS_LOG_DEFAULT, "Received object is not data");
-			return;
-		}
+	// Create handler for new connections
+	xpc_connection_set_event_handler(connection, ^(xpc_object_t peer) {
+		os_log_debug(OS_LOG_DEFAULT, "Received new peer: %p", peer);
 
-		// Parse data
-		RamjetInfo receivedInfo;
-		size_t receivedSize = xpc_data_get_bytes(object, &receivedInfo, 0, sizeof(receivedInfo));
+		// Create handler for data
+		xpc_connection_set_event_handler(connection, ^(xpc_object_t event) {
+			// Set defaults
+			kern_return_t error = KERN_SUCCESS;
 
-		// So it can stop bugging me
-		os_log_debug(OS_LOG_DEFAULT, "Got size %zu", receivedSize);
+			if (event == XPC_ERROR_CONNECTION_INVALID) {
+				// Invalid connection, cant do anything
+				os_log_error(OS_LOG_DEFAULT, "Connection is invalid");
+				return;
+			}
 
-		// Update limit
-		updateTaskLimit(receivedInfo.memorySize, receivedInfo.requester, receivedInfo.pid);
+			if (xpc_get_type(event) != XPC_TYPE_DATA) {
+				// Not data, cant parse
+				error = KERN_ABORTED;
+			} else {
+				// Parse data
+				RamjetInfo receivedInfo;
+				size_t receivedSize = xpc_data_get_bytes(event, &receivedInfo, 0, sizeof(receivedInfo));
+
+				// So it can stop bugging me
+				os_log_debug(OS_LOG_DEFAULT, "Got size %zu", receivedSize);
+
+				// Update limit
+				error = updateTaskLimit(receivedInfo.memorySize, receivedInfo.requester, receivedInfo.pid);
+			}
+
+			// Send response
+			xpc_object_t response = xpc_int64_create(error);
+			xpc_connection_send_message(peer, response);
+		});
+
+		// Resume connection
+		xpc_connection_resume(peer);
 	});
 
-	xpc_connection_resume(connection);
+	// Run forever
+	dispatch_main();
 
 	// Should never reach this point
 	exit(EXIT_FAILURE);
